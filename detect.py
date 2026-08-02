3d674a9a794b92610a21f7109d39830f2a8f56eb
anjana55413@gmail.com <anjana55413@gmail.com>
2026-08-02 16:55:27 +0530

    Completed YOLOv8 training and evaluation

--- detect.py
+++ detect.py
@@ -1,12 +1,38 @@
+# from ultralytics import YOLO
+
+# # Load your trained model
+# model = YOLO("models/best.pt")
+
+# # Predict on an image
+# results = model.predict(
+#     source="images/crowd.png",   # Change this to your image name
+#     conf=0.25,
+#     save=True,
+#     show=True
+# )
+
+# print("Detection completed!")
+
+
 from ultralytics import YOLO
+import os
 
-model = YOLO("yolov8n.pt")
+print("Current folder:", os.getcwd())
 
-results = model(
-    source="dataset/test/images",
-    classes=[0],      # Only detect persons
-    conf=0.5,
-    save=True
+print("Loading model...")
+model = YOLO("models/best.pt")
+print("Model loaded!")
+
+image_path = "images/crowd.png"
+
+print("Image exists:", os.path.exists(image_path))
+
+print("Running prediction...")
+results = model.predict(
+    source=image_path,
+    conf=0.25,
+    save=True,
+    verbose=True
 )
 
-print("Detection completed!")
+print("Prediction completed!")
